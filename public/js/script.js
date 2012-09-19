ce40855f4f49cc65303f98b636df74efecf29204
eparan <eparan@gmail.com>
2012-09-19 19:53:25 +0900

    update js

--- public/js/script.js
+++ public/js/script.js
@@ -68,7 +68,7 @@ $(function(){
 */
 
 // Our overall **AppView** is the top-level piece of UI.
-  AppView = Backbone.Views.extend({
+  AppView = Backbone.View.extend({
 
     // Instead of generating a new element, bind to the existing skeleton of 
     // the App already present in the HTML.
