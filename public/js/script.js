d97fc06ed5e29f78960740d0f9de066996965b4d
eparan <eparan@gmail.com>
2012-09-20 06:50:36 +0900

    done function

--- public/js/script.js
+++ public/js/script.js
@@ -39,7 +39,7 @@ $(function(){
 
     // Filter down the list of all todo items that are finished.
     done : function(){
-      return this.filter(function(todo){return todo.get("done");});
+      return this.filter(function(todo){return todo.get('done');});
     },
 
     // Filter down the list to only todo items that are still not finished.
